6a8120a2a13dd7ea9bbed0ea8c674396e694994d
Brian Tan <briantanw@gmail.com>
2017-02-09 21:49:02 -0800

    Completed Lesson01

--- Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -17,8 +17,11 @@ package com.example.android.favoritetoys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView toysListTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
